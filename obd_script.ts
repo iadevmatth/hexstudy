d8adb0a2b3536c400c28505ecb2ede34ed89d755
MatthSil <106295362+iadevmatth@users.noreply.github.com>
2025-07-23 01:08:48 -0300

    feature: debug hexadecimal code

---
+++ obd_script.ts
@@ -0,0 +1,65 @@
+4040 //  Cabeçalho do pacote
+
+8600 //  Tamanho do pacote
+
+04 // versão do protocolo
+
+3231384c53414232303235303030303032000000 //  ID do dispositivo
+
+1001 //  Protocolo ID
+
+508e7a68 // Última vez que o carro foi ligado
+
+1e997a68 // UTC-Time
+
+fea31900 // Total de quilometragem do veículo
+
+69a00000 // Total da quilometragem da viagem atual
+
+2b480000 // Total de combustível consumido
+
+4101 // Combustível atual
+
+00020400 // Estado do motor
+
+03 37 29 44 // (necessário converter para hexadecimal)
+
+1100831c01120719 // Reservado, 11
+
+                    00 // voltagem do veículo
+
+                    83 // frequencia da rede
+
+                    31 // Código do aparelho
+
+                    c0 // força do sinal do aparelho
+
+                    11 // Operadora do chip
+
+                    20719 // (necessário converter para hexadecimal)
+
+12 // contador GPS
+
+3923c8 // Data
+
+3bee04 // Hora
+
+2cf84d09 // latitude
+
+23024a06 // longitude
+
+dc42 // velocidade do veículo
+
+342e // direção do veículo
+
+332e392e325f42524c20323032342d30312d3235 // versão do sistema
+
+20303100442d3231384c53412d4220204844432d // versão do dispositivo
+
+3336 // novo parametro
+
+5600 //0056 alertas smsm
+
+0000 // 0000 alerta telefone
+
+d88b0d0a //  Dados do pacote
